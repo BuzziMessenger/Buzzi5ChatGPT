ce6bf2d511357cbe012b9d19fb8be2cf0a81bc20
JouwNaam <jouwemail@voorbeeld.nl>
2026-05-28 23:36:41 +0200

    Buzzi Messenger Update

--- client/src/components/ChatWindow.jsx
+++ client/src/components/ChatWindow.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export default function ChatWindow() {
   return (
     <div className="chat-window">
--- client/src/components/MessageInput.jsx
+++ client/src/components/MessageInput.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export default function MessageInput() {
   return (
     <div className="input-bar">
