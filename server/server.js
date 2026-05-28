023a403fb4f04d4d294ec02cf78144a89ef5bcc8
JouwNaam <jouwemail@voorbeeld.nl>
2026-05-29 01:50:20 +0200

    Buzzi Messenger Update

--- server/server.js
+++ server/server.js
@@ -15,8 +15,12 @@ const io = new Server(server, {
 const users = {};
 const messages = {};
 
-function key(a, b) {
+function roomKey(a, b) {
   return [a, b].sort().join("-");
+}
+
+function clean(t) {
+  return (t || "").replace(/</g, "").replace(/>/g, "");
 }
 
 io.on("connection", (socket) => {
@@ -37,28 +41,34 @@ io.on("connection", (socket) => {
 
   socket.on("set_status_text", ({ user, text }) => {
     if (users[user]) {
-      users[user].text = text;
+      users[user].text = clean(text);
       io.emit("users", users);
     }
   });
 
   socket.on("chat_message", (msg) => {
-    const k = key(msg.from, msg.to);
+    const key = roomKey(msg.from, msg.to);
 
-    if (!messages[k]) messages[k] = [];
-    messages[k].push(msg);
+    if (!messages[key]) messages[key] = [];
+
+    const fullMsg = {
+      ...msg,
+      time: Date.now()
+    };
+
+    messages[key].push(fullMsg);
 
     const target = users[msg.to];
 
     if (target) {
-      io.to(target.id).emit("chat_message", msg);
+      io.to(target.id).emit("chat_message", fullMsg);
     }
 
-    socket.emit("chat_message", msg);
+    socket.emit("chat_message", fullMsg);
   });
 
   socket.on("get_history", ({ userA, userB }) => {
-    socket.emit("history", messages[key(userA, userB)] || []);
+    socket.emit("history", messages[roomKey(userA, userB)] || []);
   });
 
   socket.on("disconnect", () => {
@@ -72,4 +82,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => console.log("MSN CLEAN SERVER RUNNING"));
+server.listen(3000, () => console.log("MSN PRO SERVER RUNNING"));
