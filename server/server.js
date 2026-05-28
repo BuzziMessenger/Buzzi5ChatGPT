297fbe5c73af7cdb1dff93e7b9991728c8f35d38
JouwNaam <jouwemail@voorbeeld.nl>
2026-05-29 01:08:31 +0200

    Buzzi Messenger Update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const cors = require("cors");
 const http = require("http");
+const cors = require("cors");
 const { Server } = require("socket.io");
 
 const app = express();
@@ -12,45 +12,16 @@ const io = new Server(server, {
   cors: { origin: "*" }
 });
 
-// username -> socket.id
-const users = {};
-
 io.on("connection", (socket) => {
   console.log("connected:", socket.id);
 
-  // login/register
-  socket.on("register", (username) => {
-    if (!username) return;
+  socket.on("chat_message", (msg) => {
+    console.log("msg received:", msg);
 
-    socket.username = username;
-    users[username] = socket.id;
-
-    console.log("REGISTER:", username);
-    io.emit("users", Object.keys(users));
-  });
-
-  // private chat
-  socket.on("private_message", ({ from, to, text }) => {
-    console.log("MSG:", from, "->", to, text);
-
-    const targetId = users[to];
-
-    if (targetId) {
-      io.to(targetId).emit("private_message", { from, text });
-    }
-
-    socket.emit("private_message", { from, text });
-  });
-
-  // disconnect
-  socket.on("disconnect", () => {
-    if (socket.username) {
-      delete users[socket.username];
-      io.emit("users", Object.keys(users));
-    }
+    io.emit("chat_message", msg); // broadcast naar iedereen
   });
 });
 
 server.listen(process.env.PORT || 3000, () => {
-  console.log("Server running");
+  console.log("running");
 });
