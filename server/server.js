5887da38499d3c1c07a5453eb04c13df81850ee6
JouwNaam <jouwemail@voorbeeld.nl>
2026-05-29 00:48:33 +0200

    Buzzi Messenger Update

--- server/server.js
+++ server/server.js
@@ -16,54 +16,37 @@ const users = {};
 
 io.on("connection", (socket) => {
 
-  console.log("🔌 connected:", socket.id);
-
   socket.on("register", (username) => {
     users[username] = socket.id;
     socket.username = username;
 
-    console.log("👤 register:", username);
     io.emit("users", Object.keys(users));
+    console.log("User online:", username);
   });
 
   socket.on("private_message", (data) => {
-    console.log("📩 RECEIVED:", data);
-
     const { from, to, text } = data;
+
+    console.log("MSG:", from, "->", to, text);
 
     const target = users[to];
 
-    if (!target) {
-      console.log("❌ user not found:", to);
-      return;
+    if (target) {
+      io.to(target).emit("private_message", { from, text });
     }
 
-    console.log("➡ sending to:", to);
-
-    io.to(target).emit("private_message", {
-      from,
-      text
-    });
-
-    socket.emit("private_message", {
-      from,
-      text
-    });
+    socket.emit("private_message", { from, text });
   });
 
   socket.on("disconnect", () => {
-    console.log("❌ disconnect");
-
-    for (const name in users) {
-      if (users[name] === socket.id) {
-        delete users[name];
-      }
+    if (socket.username) {
+      delete users[socket.username];
+      io.emit("users", Object.keys(users));
     }
-
-    io.emit("users", Object.keys(users));
   });
+
 });
 
 server.listen(process.env.PORT || 3000, () => {
-  console.log("🚀 MSN server running");
+  console.log("Server running");
 });
