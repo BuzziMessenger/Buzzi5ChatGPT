b3743f11b713041123c96d8824b6086348f7dc13
JouwNaam <jouwemail@voorbeeld.nl>
2026-05-29 00:39:53 +0200

    Buzzi Messenger Update

--- server/server.js
+++ server/server.js
@@ -12,17 +12,51 @@ const io = new Server(server, {
   cors: { origin: "*" }
 });
 
+// username -> socket.id
+const users = {};
+
+function getOnlineUsers() {
+  return Object.keys(users);
+}
+
 io.on("connection", (socket) => {
 
-  socket.on("private_message", (data) => {
+  // USER REGISTRATION
+  socket.on("register", (username) => {
+    users[username] = socket.id;
+    socket.username = username;
 
-    // stuur naar iedereen (simpel model)
-    io.emit("private_message", data);
-
+    io.emit("users", getOnlineUsers());
   });
 
+  // PRIVATE MESSAGE ROUTING
+  socket.on("private_message", ({ to, from, text }) => {
+
+    const targetSocketId = users[to];
+
+    if (targetSocketId) {
+      io.to(targetSocketId).emit("private_message", {
+        from,
+        text
+      });
+    }
+
+    // send back to sender too (so chat sync works)
+    socket.emit("private_message", {
+      from,
+      text
+    });
+  });
+
+  // DISCONNECT
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      delete users[socket.username];
+      io.emit("users", getOnlineUsers());
+    }
+  });
 });
 
 server.listen(process.env.PORT || 3000, () => {
-  console.log("server running");
+  console.log("MSN server running");
 });
