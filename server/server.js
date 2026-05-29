8f07f77a54ad0ba2d2ef91222b2f16b67a9e4699
JouwNaam <jouwemail@voorbeeld.nl>
2026-05-29 02:55:47 +0200

    Buzzi Messenger Update

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 io.on("connection", (socket) => {
 
+  console.log("user connected");
+
   socket.on("login_success", (u) => {
     socket.username = u.username;
-    socket.join(u.username); // 🔥 BELANGRIJK (fixes everything)
+    socket.join(u.username);
   });
 
+  // USERS UPDATE (optioneel bestaande logica laten)
+  socket.on("get_users", async () => {
+    // jouw bestaande DB users emit hier
+  });
+
+  // CHAT MESSAGE ROUTING
+  socket.on("chat_message", (data) => {
+    io.to(data.to).emit("chat_message", data);
+    io.to(data.from).emit("chat_message", data);
+  });
+
+  // TYPING
   socket.on("typing", ({ from, to }) => {
     io.to(to).emit("typing", { from });
   });
@@ -13,6 +27,7 @@ io.on("connection", (socket) => {
     io.to(to).emit("stop_typing", { from });
   });
 
+  // SEEN
   socket.on("message_seen", ({ from, to }) => {
     io.to(to).emit("message_seen", { from, to });
   });
