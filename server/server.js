b06c37a659b77405776a6a82a60618f31a15f500
JouwNaam <jouwemail@voorbeeld.nl>
2026-05-29 04:38:02 +0200

    Buzzi Messenger Update

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+```js
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
@@ -10,66 +11,109 @@ const io = new Server(server, {
   cors: { origin: "*" }
 });
 
-mongoose.connect("mongodb+srv://Buzzi:BuzziMessenger@buzzimessenger.yoprloo.mongodb.net/buzzi_db?retryWrites=true&w=majority");
+mongoose.connect(
+  "mongodb+srv://Buzzi:BuzziMessenger@buzzimessenger.yoprloo.mongodb.net/buzzi_db?retryWrites=true&w=majority"
+);
+
+/* =========================
+   MODELS
+========================= */
 
 const User = mongoose.model("User", {
-  username: { type: String, unique: true },
+  username: String,
   password: String,
-  status: String
+  status: String,
+  avatar: String
 });
 
 const Message = mongoose.model("Message", {
   from: String,
   to: String,
   text: String,
-  time: Number
+  time: Number,
+  read: Boolean
 });
+
+/* =========================
+   ONLINE USERS
+========================= */
+
+const onlineUsers = {};
+
+/* =========================
+   SOCKET
+========================= */
 
 io.on("connection", (socket) => {
 
   socket.on("auth", async ({ user, pass, mode }) => {
 
-    try {
-
-      if (!user || !pass) {
-        return socket.emit("error_msg", "Vul alle velden in");
-      }
-
-      if (mode === "register") {
-        const exists = await User.findOne({ username: user });
-        if (exists) return socket.emit("error_msg", "Gebruiker bestaat al");
-
-        await User.create({ username: user, password: pass, status: "online" });
-      }
-
-      const found = await User.findOne({ username: user });
-
-      if (!found || found.password !== pass) {
-        return socket.emit("error_msg", "Onjuiste login gegevens");
-      }
-
-      socket.username = user;
-      socket.join(user);
-
-      await User.updateOne({ username: user }, { status: "online" });
-
-      socket.emit("login_ok", { username: user });
-
-      const users = await User.find();
-      io.emit("users", users);
-
-    } catch (e) {
-      socket.emit("error_msg", "Server fout");
+    if (!user || !pass) {
+      return socket.emit("error_msg", "Vul alle velden in");
     }
+
+    let found = await User.findOne({ username: user });
+
+    if (mode === "register") {
+
+      if (found) {
+        return socket.emit("error_msg", "Gebruiker bestaat al");
+      }
+
+      found = await User.create({
+        username: user,
+        password: pass,
+        status: "online",
+        avatar: "https://api.dicebear.com/7.x/bottts/svg?seed=chat1"
+      });
+
+    }
+
+    if (!found || found.password !== pass) {
+      return socket.emit("error_msg", "Onjuiste login");
+    }
+
+    socket.username = found.username;
+
+    onlineUsers[found.username] = socket.id;
+
+    socket.join(found.username);
+
+    await User.updateOne(
+      { username: found.username },
+      { status: "online" }
+    );
+
+    socket.emit("login_ok", found);
+
+    const users = await User.find();
+
+    io.emit("users", users);
+
   });
 
-  socket.on("msg", async (data) => {
-    const msg = await Message.create(data);
-    io.to(data.to).emit("msg", msg);
-    io.to(data.from).emit("msg", msg);
+  /* =========================
+     BERICHTEN
+  ========================= */
+
+  socket.on("msg", async (m) => {
+
+    const msg = await Message.create({
+      ...m,
+      read: false
+    });
+
+    io.to(m.to).emit("msg", msg);
+    io.to(m.from).emit("msg", msg);
+
   });
+
+  /* =========================
+     HISTORY
+  ========================= */
 
   socket.on("history", async ({ a, b }) => {
+
     const msgs = await Message.find({
       $or: [
         { from: a, to: b },
@@ -78,8 +122,75 @@ io.on("connection", (socket) => {
     });
 
     socket.emit("history", msgs);
+
+  });
+
+  /* =========================
+     TYPING
+  ========================= */
+
+  socket.on("typing", ({ to, from }) => {
+    io.to(to).emit("typing", from);
+  });
+
+  /* =========================
+     STATUS
+  ========================= */
+
+  socket.on("status_update", async ({ user, status }) => {
+
+    await User.updateOne(
+      { username: user },
+      { status }
+    );
+
+    const users = await User.find();
+
+    io.emit("users", users);
+
+  });
+
+  /* =========================
+     AVATAR
+  ========================= */
+
+  socket.on("avatar_update", async ({ user, avatar }) => {
+
+    await User.updateOne(
+      { username: user },
+      { avatar }
+    );
+
+    const users = await User.find();
+
+    io.emit("users", users);
+
+  });
+
+  /* =========================
+     DISCONNECT
+  ========================= */
+
+  socket.on("disconnect", async () => {
+
+    if (!socket.username) return;
+
+    delete onlineUsers[socket.username];
+
+    await User.updateOne(
+      { username: socket.username },
+      { status: "offline" }
+    );
+
+    const users = await User.find();
+
+    io.emit("users", users);
+
   });
 
 });
 
-server.listen(10000, () => console.log("BUZZI PRO ONLINE"));
+server.listen(10000, () => {
+  console.log("BUZZI FINAL ONLINE");
+});
+```
