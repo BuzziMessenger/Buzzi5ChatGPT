211293363cee813f9c9f7ffd586332e1f5d84003
JouwNaam <jouwemail@voorbeeld.nl>
2026-05-29 03:04:14 +0200

    Buzzi Messenger Update

--- server/server.js
+++ server/server.js
@@ -7,54 +7,35 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET","POST"]
-  }
+  cors: { origin: "*" }
 });
 
-/* ================= DB ================= */
 mongoose.connect("YOUR_MONGO_URI");
 
-const UserSchema = new mongoose.Schema({
+const User = mongoose.model("User", {
   username: { type: String, unique: true },
   password: String
 });
 
-const User = mongoose.model("User", UserSchema);
-
-/* ================= SOCKET ================= */
 io.on("connection", (socket) => {
 
-  console.log("user connected");
+  console.log("connected");
 
-  socket.on("register", async (data) => {
-
-    const { user, pass, mode } = data;
+  socket.on("register", async ({ user, pass, mode }) => {
 
     if (!user || !pass) return;
 
     try {
 
       if (mode === "register") {
-
         const exists = await User.findOne({ username: user });
-
-        if (exists) {
-          socket.emit("login_error", { msg: "User bestaat al" });
-          return;
-        }
+        if (exists) return socket.emit("login_error", "Bestaat al");
 
         await User.create({ username: user, password: pass });
-
       }
 
       const found = await User.findOne({ username: user, password: pass });
-
-      if (!found) {
-        socket.emit("login_error", { msg: "Login fout" });
-        return;
-      }
+      if (!found) return socket.emit("login_error", "Fout login");
 
       socket.username = user;
       socket.join(user);
@@ -69,7 +50,6 @@ io.on("connection", (socket) => {
   });
 
   socket.on("chat_message", (data) => {
-
     io.to(data.to).emit("chat_message", data);
     io.to(data.from).emit("chat_message", data);
   });
@@ -82,25 +62,15 @@ io.on("connection", (socket) => {
     io.to(to).emit("stop_typing", { from });
   });
 
-  socket.on("message_seen", ({ from, to }) => {
-    io.to(to).emit("message_seen", { from, to });
-  });
-
-  /* USERS LIST */
-  async function sendUsers() {
-
-    const users = await User.find();
-
-    io.emit("users",
-      users.map(u => ({
+  function sendUsers() {
+    User.find().then(users => {
+      io.emit("users", users.map(u => ({
         username: u.username,
         status: "online"
-      }))
-    );
+      })));
+    });
   }
 
 });
 
-server.listen(10000, () => {
-  console.log("BUZZI RUNNING");
-});
+server.listen(10000, () => console.log("BUZZI SNAPSHOT RUNNING"));
