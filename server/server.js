a0e549509ac762860f935f985fbfa74f2a6aed06
JouwNaam <jouwemail@voorbeeld.nl>
2026-05-30 14:09:10 +0200

    Buzzi Messenger Update

--- server/server.js
+++ server/server.js
@@ -2,70 +2,63 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
-
-app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Buzzi Messenger Server Online");
-});
-
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
+    origin: "*"
   }
 });
 
-const users = {};
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// 🔌 MongoDB connectie
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB verbonden"))
+  .catch(err => console.log("Mongo error:", err));
+
+// 📦 Message schema + model
+const MessageSchema = new mongoose.Schema({
+  from: String,
+  to: String,
+  text: String,
+  time: Number
+});
+
+const Message = mongoose.model("Message", MessageSchema);
+
+// 📡 Socket connect
 io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-  console.log("Connected:", socket.id);
-
-  socket.on("login", (username) => {
-
-    users[socket.id] = {
-      id: socket.id,
-      username: username,
-      status: "Online"
-    };
-
-    io.emit("users", Object.values(users));
-
-    socket.emit("login_ok", {
-      username: username
+  socket.on("send_message", async (data) => {
+    const message = new Message({
+      from: data.from,
+      to: data.to,
+      text: data.text,
+      time: Date.now()
     });
 
-  });
+    await message.save();
 
-  socket.on("msg", (message) => {
-    io.emit("msg", message);
-  });
-
-  socket.on("buzz", (data) => {
-    io.emit("buzz", data);
-  });
-
-  socket.on("wink", (data) => {
-    io.emit("wink", data);
+    io.emit("receive_message", message);
   });
 
   socket.on("disconnect", () => {
-
-    delete users[socket.id];
-
-    io.emit("users", Object.values(users));
-
+    console.log("User disconnected");
   });
-
 });
 
-const PORT = process.env.PORT || 10000;
-
-server.listen(PORT, () => {
-  console.log("Server gestart op poort", PORT);
+// 🚀 start server
+server.listen(3000, () => {
+  console.log("Server draait op http://localhost:3000");
 });
