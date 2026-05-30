2c40453ce5c4def238cad911ebe904c78c529aa9
JouwNaam <jouwemail@voorbeeld.nl>
2026-05-30 14:54:44 +0200

    Buzzi Messenger Update

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const multer = require("multer");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -14,53 +17,113 @@ const io = new Server(server, {
 
 app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
 mongoose.connect(process.env.MONGO_URL);
 
-/* MESSAGE MODEL (ENCRYPTED READY) */
+/* STORAGE (AVATARS) */
+const storage = multer.diskStorage({
+  destination: "./uploads",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage });
+
+/* MODELS */
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  avatar: String
+});
+
 const MessageSchema = new mongoose.Schema({
   from: String,
   to: String,
-  encrypted: String,
-  time: Number
+  text: String,
+  time: Number,
+  seen: Boolean
 });
 
+const User = mongoose.model("User", UserSchema);
 const Message = mongoose.model("Message", MessageSchema);
 
 /* ONLINE USERS */
 let users = {};
-
-/* SIMPLE KEY STORE (IN MEMORY) */
-let publicKeys = {};
+let typingUsers = {};
 
 io.on("connection", (socket) => {
 
-  /* JOIN */
-  socket.on("join", (user) => {
-    users[socket.id] = user.name;
+  /* 👤 REGISTER */
+  socket.on("register", async ({ username, password }) => {
+    const hash = await bcrypt.hash(password, 10);
+
+    await User.create({
+      username,
+      password: hash,
+      avatar: ""
+    });
+
+    socket.emit("sound", "login");
+  });
+
+  /* 🔑 LOGIN */
+  socket.on("login", async ({ username, password }) => {
+    const user = await User.findOne({ username });
+    if (!user) return socket.emit("login_failed");
+
+    const ok = await bcrypt.compare(password, user.password);
+    if (!ok) return socket.emit("login_failed");
+
+    users[socket.id] = username;
+
     io.emit("users_update", Object.values(users));
+
+    socket.emit("login_success", {
+      username,
+      avatar: user.avatar
+    });
+
+    socket.emit("sound", "login");
   });
 
-  /* 🔐 PUBLIC KEY EXCHANGE (E2EE BASIS) */
-  socket.on("set_key", ({ user, key }) => {
-    publicKeys[user] = key;
-  });
-
-  socket.on("get_key", (user, cb) => {
-    cb(publicKeys[user] || null);
+  /* 🖼 UPLOAD AVATAR */
+  app.post("/avatar", upload.single("avatar"), async (req, res) => {
+    const url = `https://buzzimessenger.onrender.com/uploads/${req.file.filename}`;
+    res.json({ url });
   });
 
   /* 💬 MESSAGE */
   socket.on("send_message", async (msg) => {
-    const saved = await Message.create({
+    const m = await Message.create({
       ...msg,
-      time: Date.now()
+      time: Date.now(),
+      seen: false
     });
 
-    io.emit("receive_message", saved);
+    io.emit("receive_message", m);
+    io.emit("sound", "message");
   });
 
-  /* DISCONNECT */
+  /* ✍ TYPING */
+  socket.on("typing", ({ user, to, isTyping }) => {
+    typingUsers[user] = isTyping;
+
+    io.emit("typing_update", {
+      user,
+      isTyping
+    });
+  });
+
+  /* 👁 SEEN */
+  socket.on("seen", async ({ from, to }) => {
+    await Message.updateMany(
+      { from, to, seen: false },
+      { seen: true }
+    );
+  });
+
+  /* ❌ DISCONNECT */
   socket.on("disconnect", () => {
     delete users[socket.id];
     io.emit("users_update", Object.values(users));
@@ -68,5 +131,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(3000, () => {
-  console.log("Buzzi Messenger 10.0 NO-REDIS running");
+  console.log("Buzzi Messenger 11.0 FINAL POLISH running");
 });
