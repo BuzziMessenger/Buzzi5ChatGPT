f14c3acb5a28dbd411148e8626035e239edb15db
JouwNaam <jouwemail@voorbeeld.nl>
2026-05-30 02:25:30 +0200

    Buzzi Messenger Update

--- server/server.js
+++ server/server.js
@@ -1,75 +1,33 @@
-import express from "express";
-import http from "http";
-import cors from "cors";
-import { Server } from "socket.io";
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const socket = io("https://buzzimessenger.onrender.com", {
+  transports: ["websocket", "polling"]
+});
 
 const app = express();
 app.use(cors());
-app.use(express.static("public"));
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
-const users = new Map();   // username -> {id, status}
-const messages = [];       // simple memory
-
-function emitUsers(){
-  io.emit("users", Array.from(users.entries()).map(([name,u])=>({
-    username:name,
-    status:u.status
-  })));
-}
-
 io.on("connection", (socket) => {
+  console.log("user connected");
 
-  socket.on("login", (username) => {
-    socket.username = username;
-
-    users.set(username, {
-      id: socket.id,
-      status: "online"
-    });
-
-    emitUsers();
-
-    socket.emit("history", messages);
+  socket.on("msg", (data) => {
+    io.emit("msg", data);
   });
 
   socket.on("disconnect", () => {
-    if (!socket.username) return;
-
-    const u = users.get(socket.username);
-    if (u) u.status = "offline";
-
-    emitUsers();
+    console.log("user disconnected");
   });
-
-  // CHAT
-  socket.on("msg", ({from,to,text}) => {
-    const msg = {from,to,text,time:Date.now()};
-    messages.push(msg);
-
-    const target = users.get(to);
-
-    if (target?.id) io.to(target.id).emit("msg", msg);
-    io.to(socket.id).emit("msg", msg);
-  });
-
-  // BUZZ (MSN NUDGE)
-  socket.on("buzz", ({to}) => {
-    const t = users.get(to);
-    if (t?.id) io.to(t.id).emit("buzz");
-  });
-
-  // WINK
-  socket.on("wink", ({to,emoji}) => {
-    const t = users.get(to);
-    if (t?.id) io.to(t.id).emit("wink", emoji);
-  });
-
 });
 
-server.listen(10000, () => console.log("MSN clone running"));
+server.listen(process.env.PORT || 10000);
